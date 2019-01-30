4a4091ce33fc405d318d80ee2361192891abf8b5
Noah Scholfield <contact@noahscholfield.com>
2019-01-30 16:13:14 -0500

    Add text, remove other map

--- trees/mapbox.js
+++ trees/mapbox.js
@@ -35,39 +35,3 @@ map1.on('click', function(e) {
     .setLngLat(feature.geometry.coordinates)
     .addTo(map1);
 });
-/*
-const map2 = new mapboxgl.Map({
-  container: 'healthmap',
-  style: 'mapbox://styles/njscholfield/cjrgw8f1x23yp2sl7qycx50oy',
-  center: [-79.984326, 40.441071],
-  zoom: 14.0
-});
-
-map2.addControl(new mapboxgl.NavigationControl());
-map2.addControl(new mapboxgl.GeolocateControl({
-  positionOptions: {
-    enableHighAccuracy: true
-  },
-  trackUserLocation: true
-}));
-
-map2.on('click', function(e) {
-  var features = map2.queryRenderedFeatures(e.point, {
-    layers: ['tree-dots']
-  });
-
-  if (!features.length) {
-    return;
-  }
-
-  var feature = features[0];
-  var props = feature.properties;
-  var benefits = (props.overall_benefits_dollar_value) ? `$${Math.round(props.overall_benefits_dollar_value)}` : 'N/A';
-
-  var popup = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(feature.geometry.coordinates)
-    .setHTML(`<h3>${props.common_name}</h3><p><strong>Condition:</strong> ${props.condition}</p><p><strong>Diameter:</strong> ${props.diameter_base_height}</p><p><strong>Overall Value:</strong> ${benefits}</p>`)
-    .setLngLat(feature.geometry.coordinates)
-    .addTo(map2);
-});
-*/
