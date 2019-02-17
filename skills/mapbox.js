1f4cb67f1fb5f5209810c66ceaf24355af8c2dbb
Noah Scholfield <contact@noahscholfield.com>
2019-02-16 23:07:20 -0500

    Let users add a title to the locations they add

--- skills/mapbox.js
+++ skills/mapbox.js
@@ -45,18 +45,57 @@ map.on('load', function() {
   });
 });
 
+let modal = document.querySelector('.modal-backdrop');
+let geometry = [];
+
 geocoder.on('result', function(e) {
+  modal.classList.remove('d-none');
+  document.querySelector('input[name="title"]').focus();
+  geometry = e.result.geometry;
+});
+
+function addPoint(e) {
+  e.preventDefault();
+  modal.classList.add('d-none');
   const feature = {
     type: 'Feature',
-    geometry: e.result.geometry
+    properties: {
+      title: e.currentTarget[0].value,
+    },
+    geometry,
   };
   points.features.push(feature);
   map.getSource('points').setData(points);
   localStorage.setItem('points', JSON.stringify(points.features));
-});
+}
+document.querySelector('.modal-backdrop form').addEventListener('submit', addPoint);
 
 document.getElementById('js-clear').addEventListener('click', function() {
   points.features = [];
   map.getSource('points').setData(points);
   localStorage.setItem('points', JSON.stringify([]));
+});
+
+function closeModal() {
+  document.querySelector('.modal-backdrop').classList.add('d-none');
+}
+for (const el of document.getElementsByClassName('mdl-close')) {
+  el.addEventListener('click', closeModal);
+}
+
+map.on('click', function(e) {
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ['points']
+  });
+
+  if (!features.length) {
+    return;
+  }
+
+  var feature = features[0];
+
+  var popup = new mapboxgl.Popup({ offset: [0, -15] })
+    .setLngLat(feature.geometry.coordinates)
+    .setText(feature.properties.title)
+    .addTo(map);
 });
